f57c874fb32b6db099448ca826b0a03c4ef46975
Sergey Matyukevich <geomatsi@gmail.com>
2026-09-19 16:12:56 +0300

    examples: simplify gen-test1
    Simplify gen-test1 to set a single frequency with the same standard
    si5351 settings that are used by wspr beacon. This example is handy
    for testing the board output using differnt tools, e.g. tinySA.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- tools/wspr-beacon--blue-pill/examples/gen-test1.rs
+++ tools/wspr-beacon--blue-pill/examples/gen-test1.rs
@@ -4,14 +4,16 @@
 use cortex_m as cm;
 use cortex_m_rt::entry;
 use panic_semihosting as _;
-use si5351::{ClockOutput, Frequency, PLL, Si5351, Si5351Device};
+use si5351::{ClockOutput, Frequency, PLL, Si5351, Si5351Device, DriveStrength};
+use stm32f1xx_hal::gpio::{ErasedPin, Output, PushPull};
 use stm32f1xx_hal::pac;
 use stm32f1xx_hal::rcc;
-
 use stm32f1xx_hal::{
     i2c::{DutyCycle, Mode},
     prelude::*,
 };
+
+use wspr_beacon::beacon::config;
 
 #[entry]
 fn main() -> ! {
@@ -22,10 +24,18 @@ fn main() -> ! {
         rcc::Config::hse(8.MHz()).sysclk(32.MHz()).pclk1(16.MHz()),
         &mut flash.acr,
     );
-    let mut afio = dp.AFIO.constrain(&mut rcc);
 
-    let gpiob = dp.GPIOB.split(&mut rcc);
+    let mut afio = dp.AFIO.constrain(&mut rcc);
+    let mut gpiob = dp.GPIOB.split(&mut rcc);
     let mut delay = cp.SYST.delay(&rcc.clocks);
+
+    let mut led: ErasedPin<Output<PushPull>> = match config::CFG.hw.model {
+        config::BluePill::Classic => {
+            let mut gpioc = dp.GPIOC.split(&mut rcc);
+            gpioc.pc13.into_push_pull_output(&mut gpioc.crh).erase()
+        }
+        config::BluePill::Plus => gpiob.pb2.into_push_pull_output(&mut gpiob.crl).erase(),
+    };
 
     let scl = gpiob.pb8;
     let sda = gpiob.pb9;
@@ -46,18 +56,18 @@ fn main() -> ! {
     let mut clock = Si5351Device::new_adafruit_module(i2c);
     clock.init_adafruit_module().unwrap();
 
-    let freqs = [7_000_000u32, 8_000_000u32, 9_000_000u32, 10_000_000u32];
+    let freq = 7_000_000u32; //14_000_000u32;
+
+    clock
+        .set_frequency(PLL::A, ClockOutput::Clk0, Frequency::from_hz(freq))
+        .unwrap();
+    clock.set_clock_drive(ClockOutput::Clk0, DriveStrength::_2mA);
+    clock.set_clock_enabled(ClockOutput::Clk0, true);
+    clock.flush_clock_control(ClockOutput::Clk0).unwrap();
+    clock.flush_output_enabled().unwrap();
 
     loop {
-        for freq in freqs {
-            clock
-                .set_frequency(PLL::A, ClockOutput::Clk0, Frequency::from_hz(freq))
-                .unwrap();
-            clock.set_clock_enabled(ClockOutput::Clk0, true);
-            clock.flush_clock_control(ClockOutput::Clk0).unwrap();
-            clock.flush_output_enabled().unwrap();
-
-            delay.delay_ms(1000u32);
-        }
+        delay.delay_ms(1000u32);
+        led.toggle();
     }
 }
